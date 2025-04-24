973830f3e80619e8fece7fe2456ad0665c96cc7a
JinJungs <nehciyz@gmail.com>
2025-04-24 11:59:20 -0700

    add another solution of 49

--- string/medium/49. Group Anagrams.py
+++ string/medium/49. Group Anagrams.py
@@ -11,9 +11,20 @@ class Solution:
         should be equal
         # sorted list
         """
+
+        # 1. Sorting: O(n*mlog(m))
         d = defaultdict(list)
         for s in strs:
             t = str(sorted(s))
             d[t].append(s)
 
         return list(d.values())
+    
+
+        # 2. Hash Table: O(n*m*26)
+        for s in strs: # O(n)
+            count = [0] * 26 # the key will be the count list of alphabet [0,1,2,...] -> bcc
+            for c in s: # O(m)
+                count[ord(c) - ord('a')] += 1
+            h[tuple(count)].append(s) # list cannot be key 
+        return list(h.values())
