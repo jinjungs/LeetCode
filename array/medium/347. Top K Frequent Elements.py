e9adef4eb004de2d57877dee0bc0c745eb0f677f
JinJungs <nehciyz@gmail.com>
2025-04-24 14:14:32 -0700

    fix a solution of 347
    - use bucket sort instead of sorting

--- array/medium/347. Top K Frequent Elements.py
+++ array/medium/347. Top K Frequent Elements.py
@@ -4,18 +4,35 @@ from typing import List
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # 1. Sorting: O(nlogn)
         # count the number of occurance in hashmap
-        d = defaultdict(int)
-        for num in nums:
-            d[num] += 1
+        # d = defaultdict(int)
+        # for num in nums:
+        #     d[num] += 1
         
-        # how to return k most frequent element?
-        rest = k
-        result = []
-        for key, value in sorted(d.items(), key=lambda item: item[1], reverse=True):
-            result.append(key)
-            rest -=1
-            if rest <=0:
-                break
+        # # how to return k most frequent element?
+        # rest = k
+        # result = []
+        # for key, value in sorted(d.items(), key=lambda item: item[1], reverse=True):
+        #     result.append(key)
+        #     rest -=1
+        #     if rest <=0:
+        #         break
 
-        return result
+        # return result
+
+        # 2. Bucket Sort
+        count = defaultdict(int)
+        freq = [[] for i in range(len(nums)+1)]
+
+        for num in nums:
+            count[num] += 1
+        for num, cnt in count.items():
+            freq[cnt].append(num)
+
+        res = []
+        for i in range(len(freq)- 1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
