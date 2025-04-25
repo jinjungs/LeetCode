332366c24da3f747ba504e80f500aad17c650afb
JinJungs <nehciyz@gmail.com>
2025-04-25 07:55:29 -0700

    fix a solution of 1

--- hash/easy/1. Two Sum.py
+++ hash/easy/1. Two Sum.py
@@ -3,10 +3,15 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        temp = dict()
-        for i in range(len(nums)):
-            num = nums[i]
-            if target - num in temp.keys():
-                return [temp[target-num], i]
-            else:
-                temp[num] = i
+        # Test all cases: O(n^2)
+        # for i in range(len(nums)):
+        #     for j in range(1,len(nums)):
+        #         if i!= j and nums[i] + nums[j] == target:
+        #             return [i,j]
+
+        # Hashmap: O(n)
+        d = dict() # val -> index
+        for i, num in enumerate(nums):
+            if target - num in d:
+                return [d.get(target - num), i]
+            d[num] = i
