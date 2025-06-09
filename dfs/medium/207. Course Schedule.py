a3a34d267b34a9ba61294f62f4897b339551c339
JinJungs <nehciyz@gmail.com>
2025-06-08 23:34:40 -0700

    add another soultion of 207

--- dfs/medium/207. Course Schedule.py
+++ dfs/medium/207. Course Schedule.py
@@ -39,3 +39,37 @@ class Solution:
                 return False
 
         return True
+
+# Solved: 25/06/08
+class Solution2:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        if not prerequisites or not numCourses:
+            return True
+
+        # make map: key: course value: list of prerequisites
+        d = defaultdict(list)
+        for pre in prerequisites:
+            d[pre[0]].append(pre[1])
+
+        # detect cycle
+        visited = set()
+        def dfs(course: int) -> bool:
+            if course in visited:
+                return False
+            if len(d[course]) == 0:
+                return True
+
+            visited.add(course)
+            for pre in d[course]:
+                if not dfs(pre):
+                    return False
+            d[course] = []
+            visited.remove(course)
+            return True
+            
+        k = list(d.keys())
+        for course in k:
+            if not dfs(course):
+                return False
+        return True
+        
