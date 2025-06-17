c9c08620b4e16e87e9f51c1fd9b0e48ae373e0dd
JinJungs <nehciyz@gmail.com>
2025-06-17 13:53:43 -0700

    add other solutions of 91

--- dynamicProgramming/medium/91. Decode Ways.py
+++ dynamicProgramming/medium/91. Decode Ways.py
@@ -1,3 +1,4 @@
+# Top-Down
 class Solution:
     def numDecodings(self, s: str) -> int:
         n = len(s)
@@ -19,4 +20,40 @@ class Solution:
             return dp[i]
 
         return dfs(0)
+    
+# Bottom-Up
+class Solution2:
+    def numDecodings(self, s: str) -> int:
+        n = len(s)
+        if n == 0 or s[0] == '0':
+            return 0
+        
+        dp = {len(s): 1} # one way to decode empty strings
+        dp[n-1] = 0 if s[n-1] == '0' else 1
+
+        for i in range(n-2, -1, -1):
+            if s[i] == "0":
+                dp[i] = 0
+            else:
+                one_digit = dp[i + 1]
+                two_digit = dp[i + 2] if int(s[i:i+2]) <= 26 else 0
+                dp[i] = one_digit + two_digit
+
+        return dp[0]
                             
+# Bottom-Up (Space Optimized)
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        n = len(s)
+        dp1, dp2 = 1, 0
+
+        for i in range(n-1, -1, -1):
+            curr = 0
+            if s[i] != "0":
+                curr += dp1
+                if i + 1 < n and 10 <= int(s[i:i+2]) <= 26:
+                    curr += dp2
+            dp2 = dp1
+            dp1 = curr
+
+        return dp1
