b94a54e4cebf5c5bbeae565ad5a5e06fde79dd9f
JinJungs <nehciyz@gmail.com>
2025-06-18 14:56:02 -0700

    add other solutions of 139

--- dynamicProgramming/medium/139. Word Break.py
+++ dynamicProgramming/medium/139. Word Break.py
@@ -22,3 +22,19 @@ class Solution:
             return False
 
         return recursive(0)
+    
+# 25/06/18
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n = len(s)
+        dp = [False] * (n+1)
+        dp[n] = True
+
+        for i in range(n-1, -1, -1):
+            for w in wordDict:
+                if len(w) <= n - i and w == s[i:i+len(w)]:
+                    dp[i] = dp[i + len(w)]
+                if dp[i]:
+                    break
+
+        return dp[0]
