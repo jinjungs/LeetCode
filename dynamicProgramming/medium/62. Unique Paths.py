2c021877ec19238230d2a312356e913768aed7d8
JinJungs <nehciyz@gmail.com>
2025-06-19 11:18:56 -0700

    add another solution of 62

--- dynamicProgramming/medium/62. Unique Paths.py
+++ dynamicProgramming/medium/62. Unique Paths.py
@@ -13,3 +13,14 @@ class Solution:
             for j in range(1, n):
                 paths[i][j] = paths[i-1][j] + paths[i][j-1]
         return paths[-1][-1]
+
+# 25/06/19 
+# Space Optimized
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [1] * n
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[j] = dp[j-1] + dp[j]
+
+        return dp[n-1]
