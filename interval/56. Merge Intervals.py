e0ecaf47244a1a68823bd1320c728ee2f8aa5dcf
JinJungs <nehciyz@gmail.com>
2025-06-24 16:56:15 -0700

    add another solution of 56

--- interval/56. Merge Intervals.py
+++ interval/56. Merge Intervals.py
@@ -18,3 +18,22 @@ class Solution:
                 result.append(interval)
 
         return result
+    
+# 25/06/24
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+        res = [intervals[0]]
+
+        for i in range(1, len(intervals)):
+            start, end = intervals[i]
+            lastEnd = res[-1][1]
+
+            if lastEnd < start:
+                res.append(intervals[i])
+            else:
+                # Intervals are sorted, so once overlapping is confirmed,
+                # we only need to update the end of the last interval.
+                res[-1][1] = max(end, lastEnd)
+
+        return res
