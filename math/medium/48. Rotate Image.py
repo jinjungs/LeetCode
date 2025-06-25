40624ede5e2535b692175b03f5479ec2ef60a4dc
JinJungs <nehciyz@gmail.com>
2025-06-25 12:42:08 -0700

    add a solution of 48

---
+++ math/medium/48. Rotate Image.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        rotated = [[0] * n for _ in range(n)]
+
+        for i in range(n):
+            for j in range(n):
+                rotated[j][n-1-i] = matrix[i][j]
+
+        for i in range(n):
+            for j in range(n):            
+                matrix[i][j] = rotated[i][j]
