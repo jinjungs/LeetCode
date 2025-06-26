a444b6152abcb6f32b360c2a778caa68e7898987
JinJungs <nehciyz@gmail.com>
2025-06-26 12:02:17 -0700

    add a solution of 268

---
+++ bitManipulation/easy/268. Missing Number.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        res = len(nums)
+        for i, num in enumerate(nums):
+            res ^= i ^ num
+        return res
