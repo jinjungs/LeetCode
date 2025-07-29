7827b695cddd2b98bcf5eac977811aa4698fb3a7
jinjungs <ejjung.tech@gmail.com>
2025-07-28 18:13:10 -0700

    add another solution of 110

--- tree/easy/110. Balanced Binary Tree.py
+++ tree/easy/110. Balanced Binary Tree.py
@@ -19,4 +19,20 @@ class Solution:
 
         d, result = getDepth(root)
         return result
-
+    
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def dfs(node):
+            if not node:
+                return 0
+            left = dfs(node.left)
+            if left == -1:
+                return -1
+            right = dfs(node.right)
+            if right == -1:
+                return -1
+            if abs(right-left) > 1:
+                return -1
+            return max(left, right) + 1
+            
+        return dfs(root) != -1
