046afdbb0cd97f2ab6909952beb15d2376084efe
jinjungs <ejjung.tech@gmail.com>
2025-08-14 11:29:49 -0700

    add another solution of dijkstra
    - no visited

--- algorithm/dijkstra.py
+++ algorithm/dijkstra.py
@@ -1,5 +1,6 @@
 import heapq
 
+# Visited
 def dijkstra(graph, s):
     INF = 10**15
     dist = {v: INF for v in graph}
@@ -17,4 +18,26 @@ def dijkstra(graph, s):
             if dist[v] > d + w:
                 dist[v] = d + w
                 heapq.heappush(pq, (dist[v], v))
+    return dist
+
+# No Visited
+def dijkstra2(graph, s):
+    INF = 10**15
+    dist = {v: INF for v in graph}
+    dist[s] = 0
+    pq = [(0, s)]
+
+    while pq:
+        d, u = heapq.heappop(pq)
+
+        # Skip if we already found a shorter path to u
+        if d > dist[u]:
+            continue
+
+        for v, w in graph[u]:  # Explore all outgoing edges from u
+            nd = d + w
+            if nd < dist[v]:
+                dist[v] = nd
+                heapq.heappush(pq, (nd, v))
+
     return dist
