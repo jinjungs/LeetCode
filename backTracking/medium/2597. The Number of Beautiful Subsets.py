c1c55c42748146ebc77a82db34237e7349b48571
jinjungs <ejjung.tech@gmail.com>
2025-08-21 11:23:48 -0700

    add a solution of 2597

---
+++ backTracking/medium/2597. The Number of Beautiful Subsets.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def beautifulSubsets(self, nums: List[int], k: int) -> int:
+        def helper(i, count):
+            if i == len(nums):
+                return 1
+            
+            # skip
+            res = helper(i + 1, count)
+            # take
+            if not count[nums[i] + k] and not count[nums[i] - k]:
+                count[nums[i]] += 1
+                res += helper(i + 1, count)
+                count[nums[i]] -= 1
+            return res
+        
+        return helper(0, defaultdict(int)) -1
