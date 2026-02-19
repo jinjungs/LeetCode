6092e2c8672c2c0c4425fbb0c7133b2518ff38c4
jinjungs <ejjung.tech@gmail.com>
2026-02-19 11:53:52 -0800

    add another solution of 2115

--- dfs/medium/2115. Find All Possible Recipes from Given Supplies.py
+++ dfs/medium/2115. Find All Possible Recipes from Given Supplies.py
@@ -1,6 +1,7 @@
 from typing import Dict, List
+from collections import defaultdict, deque
 
-
+# DFS
 class Solution:
     def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
         supplySet = set(supplies)
@@ -43,3 +44,31 @@ class Solution:
             return possible
 
         return [r for r in recipes if can_make(r)]
+    
+# BFS
+class Solution:
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        req: Dict[str, List[str]] = dict(zip(recipes, ingredients))
+
+        ingToRec = defaultdict(list)
+        need = defaultdict(int)
+
+        for r in recipes:
+            for ing in req[r]:
+                need[r] = len(req[r])
+                ingToRec[ing].append(r)
+
+        q = deque(supplies)
+        res = []
+
+        while q:
+            item = q.popleft()
+
+            for r in ingToRec[item]:
+                need[r] -= 1
+                if need[r] == 0:
+                    res.append(r)
+                    q.append(r)
+
+        return res
+        
