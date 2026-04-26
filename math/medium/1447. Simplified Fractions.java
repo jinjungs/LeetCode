70560e78e1a321d8202e06cf401bf2d699838650
jinjungs <ejjung.tech@gmail.com>
2026-04-25 22:14:08 -0700

    add a solution of 1447

---
+++ math/medium/1447. Simplified Fractions.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<String> simplifiedFractions(int n) {
+        List<String> res = new ArrayList<>();
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                if (gcd(j, i) == 1) {
+                    res.add(j + "/" + i);
+                }
+            }
+        }
+
+        return res;
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
+    }
+}
